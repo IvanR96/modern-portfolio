a9948ef598910b913c74c3e30974b8aa36f82c03
IvanR96 <irios96@hotmail.com>
2025-12-08 21:38:17 -0800

    homepage navbar and starbackground added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
+import { Navbar } from "../components/Navbar"
 
 export const Home = () =>{
     return  <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -7,9 +9,10 @@ export const Home = () =>{
        <ThemeToggle />
 
        {/* Background Effects*/}
-
+        <StarBackground />
 
        {/* Navbar */}
+       <Navbar />
 
        {/* Main Content */}
 
