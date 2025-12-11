9fe5ae5793209533f2e3aca5240e178fa613f95d
IvanR96 <irios96@hotmail.com>
2025-12-11 10:11:17 -0800

    components footer and contact done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { ArrowUp, ArrowUpCircle } from "lucide-react";
+
+export const Footer = () =>{
+    return(
+        <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
+            <p className="text-sm text-muted-foreground"> &copy; {new Date().getFullYear()} Ivan Rios</p>
+
+            <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20">
+                <ArrowUpCircle />
+            </a>
+        </footer>
+    );
+}
